dc9b540a20c3b4aacff70e84a39094ca7db27833
Upasana Kakkar <upasanakakkar@UPASKAKK-RG5546.local>
2021-02-27 18:18:09 +0530

    auth guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      let check = this.isPresent();
+    if (check) {
+      return true;
+    }
+    this.router.navigate(['']);
+    return false;
+  }
+  isPresent() {
+    let status = false;
+    if (localStorage.getItem('currentUser')) {
+      status = true;
+    } else {
+      status = false;
+    }
+    return status;
+  }
+  
+}
